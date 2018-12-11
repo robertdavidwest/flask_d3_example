552c12a4cda113d77c385930bca9aa922138d01c
robertdavidwest <robert.david.west@gmail.com>
2018-12-10 20:09:45 -0500

    added flask-cors to app to handle cors err. added write to div is d3.json

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -11,6 +14,7 @@ def index():
 @app.route("/get-data")
 def get_data():
     return jsonify({"a": 1, "b": 2})
+
 
 if __name__ == '__main__':
     app.debug = True
--- static/js/data.js
+++ static/js/data.js
@@ -1,3 +1,6 @@
 
 d3.json("http://127.0.0.1:5000/get-data",
-    function(d){ console.log(d);})
+    function(d){
+        console.log(d);
+        document.getElementById("d3-write-here").innerHTML = d;
+    })
